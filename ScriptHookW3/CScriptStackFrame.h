5e80f5a0c1fcd26355c1047289d49f7049ea2882
Unknown <ali.alidoust@gmail.com>
2017-04-24 21:22:19 +0430

    Refactored exampleFunction and CScriptStackFrame

--- ScriptHookW3/CScriptStackFrame.h
+++ ScriptHookW3/CScriptStackFrame.h
@@ -2,6 +2,8 @@
 
 namespace w3 {
 	class CScriptStackFrame {
-
+	public:
+		unsigned char unknown_0[0x30];
+		int64_t unk_0x30;
 	};
 }
--- ScriptHookW3/global-funcs.h
+++ ScriptHookW3/global-funcs.h
@@ -4,7 +4,7 @@
 #include "CScriptStackFrame.h"
 namespace shw3 {
 	void exampleFunction(w3::IScriptable* pThis, w3::CScriptStackFrame& stackFrame, void* result) {
-		*((int64_t*)((char*)(&stackFrame) + 0x30)) = *((int64_t*)((char*)(&stackFrame) + 0x30)) + 1;
+		stackFrame.unk_0x30++;
 		*(int*)result = 12345678;
 	}
 }
